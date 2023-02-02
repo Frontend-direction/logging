35a58b973038dfb1178ea2321546dc8e3ba643af
Volodymyr Pankiv <vpanki@softserveinc.com>
2023-02-02 16:26:17 +0200

    Updates

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,10 @@ class Logger {
     log(str: string) {
         console.log('This is loggs', str);
     }
+
+    logError(err: any) {
+        console.error(err);
+    }
 }
 
 export default Logger;
